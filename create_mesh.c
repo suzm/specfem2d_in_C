99a9f172b649406277665855c3fb9ba4e9606acd
suzm <szmxjtu@163.com>
2016-04-16 14:52:44 +0800

    using dynamic storage, will update later

---
+++ create_mesh.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+/*
+This function is used to produced mesh file for sem2d
+*/
+#define region_width 100
+#define region_length 100
+#define row_element_num 200
+#define col_element_num 200
+
+int main(int argc, char const *argv[])
+{
+	double region_width = 100;
+	double region_length = 100;
+	double width_element_num = 200;
+	double length_element_num = 200;
+	char mesh_file_name[] = "specfem2d_mesh";
+	FILE * fp = fopen(mesh_file_name, "w");
+
+	double nod_x, nod_y;
+	int mesh_mat[, element_num]
+
+
+	return 0;
+}
